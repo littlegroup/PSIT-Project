2b3e617c573120783411004a665ab9d45cc29021
KRAM | KMN SSW <59070002@kmitl.ac.th>
2016-12-06 12:37:00 +0700

    Graph & code

--- Landslide.py
+++ Landslide.py
@@ -1,36 +1,43 @@
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+import pygal
 
 data = pd.read_csv('Landslide.csv')
-#print(data)
-
-#get data year to list
-year = data.YEAR
-list_year = []
-for i in year:
-    list_year.append(i)
-#print(list_year)
+print(data)
 
 #get data injured to list
 injured = data.INJURED
 list_injured = []
 for i in injured:
     list_injured.append(i)
-#print(list_injured)
+print(list_injured)
 
 #get data death to list
 death = data.DEATH
 list_death = []
 for i in death:
     list_death.append(i)
-#print(list_death)
+print(list_death)
 
-x = np.array(list_year)
-y1 = np.array(list_death)
-y2 = np.array(list_injured)
-plt.figure(figsize=(10,10))
-plt.bar(x,y1,color='r',width=0.5,label='Death',align='center')
-plt.bar(x,y2,color='orange',width=0.5,label='Injured',align='center')
-plt.legend(loc=1)
-plt.show()
+#get data damaged to list
+damage = data.DAMAGED
+list_damage = []
+for i in damage:
+    list_damage.append(i)
+print(list_damage)
+
+#get data year to list
+year = data.YEAR
+list_year = []
+for i in year:
+    list_year.append(i)
+print(list_year)
+
+chart = pygal.Line(title=u'Landslide', secondary_range=(0, 250, 50))
+chart.x_labels = map(int, list_year)
+chart.y_labels = map(int, range(0, 600, 100))
+chart.add('Injured', list_injured)
+chart.add('Death', list_death, secondary=True)
+chart.render_to_file("Landslide.svg")
+#Show Graph Chart_XY
